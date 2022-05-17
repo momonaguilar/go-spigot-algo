cfe2988e053903c8aaabecd51d26fde8da896ea0
Jose @ GCP <jose.for.dev@gmail.com>
2022-05-17 14:03:56 +0000

    Task - Update placeholder for test cases & test scenarios.

--- main_test.go
+++ main_test.go
@@ -7,9 +7,16 @@ import (
 	spigot "github.com/momonaguilar/go-spigot-algo"
 )
 
-func TestGetPi(t *testing.T) {
+func TestGetPiPrecision10(t *testing.T) {
 	fmt.Print("INFO: Starting test...")
 	spigot.Precision = 10
+	pi := spigot.GetPi()
+	fmt.Println("TEST: Pi value is: ", pi)
+}
+
+func TestGetPiPrecision20(t *testing.T) {
+	fmt.Print("INFO: Starting test...")
+	spigot.Precision = 20
 	pi := spigot.GetPi()
 	fmt.Println("TEST: Pi value is: ", pi)
 }
